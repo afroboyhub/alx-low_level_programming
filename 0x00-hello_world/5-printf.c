21b9cd38289bd55f9ed33a4a5003daeb5571d5a8
abel <berhanuabel@gmail.com>
2022-09-08 13:22:43 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,17 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - prints the string in the printsf function
+ *
+ * Description: using the main function
+ * this program prints with proper grammar, but the outcome is a piece of art,
+ * Return: 0
+ */
+
+int main(void)
+{
+	char str[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", str);
+	return (0);
+}
