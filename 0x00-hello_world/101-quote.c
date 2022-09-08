ae239b06f93558c5ed6aec6aede997741feb2d3f
abel <berhanuabel@gmail.com>
2022-09-08 14:54:08 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,17 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - prints the string in the printf function
+ *
+ * Description: using the main function
+ * this program prints and that piece of art is useful\" - Dora Korpar, 2015-10-19
+ * Return: 1
+ */
+
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	puts(str);
+	return (1);
+}
