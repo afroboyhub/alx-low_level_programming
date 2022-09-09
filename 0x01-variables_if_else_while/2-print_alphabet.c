97603a801579013bcb150eb67c0b0f36340832a7
abel <berhanuabel@gmail.com>
2022-09-09 12:28:03 -0700

    task 3

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -14,6 +14,7 @@ int main(void)
 
 	for (ab = 'a'; ab <= 'z'; ab++)
 		putchar(ab);
+		putchar('\n');
 
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in l_c and then U_C
+ *
+ * Description: using the main function
+ * program that prints l_c and U_C alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ab;
+
+	for (ab = 'a'; ab <= 'z'; ab++)
+		putchar(ab);
+	for (ab = 'A'; ab <= 'Z'; ab++)
+		putchar(ab);
+		putchar('\n');
+
+	return (0);
+}
