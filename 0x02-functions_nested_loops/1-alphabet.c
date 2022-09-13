b6f2ad717c2226f3f01e3d8230b90b0aaf5cf5af
abel <berhanuabel@gmail.com>
2022-09-13 05:11:38 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - prints alphabet in lowercase
+ * _putchar - prints alphabet in l_c
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+
+	putchar('\n');
+	return (0);
+}
