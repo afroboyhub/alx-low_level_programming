6fa52cf776e50faaefd4b97573e99f70ee352ab3
abel <berhanuabel@gmail.com>
2022-09-13 05:25:38 -0700

    task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,2 @@
-#include <unistd.h>
-
-/**
- * _putchar -
- * @c: The character to print putchar
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
+void print_alphabet(void);
