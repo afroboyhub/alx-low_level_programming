0fe05a84e318a0d00154dff5ef9e92a2f66da8f0
abel <berhanuabel@gmail.com>
2022-09-13 11:45:00 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * times_table - prints the time table of 9
+ */
+
+void times_table(void)
+{
+	int num, times, ans;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (times = 0; times <= 9; times++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			ans = times * num;
+
+			if (ans <= 9)
+				_putchar(' ');
+			else
+				_putchar((ans / 10) + '0');
+			_putchar((ans % 10) + '0');
+		}
+	_putchar('\n');
+	}
+}
