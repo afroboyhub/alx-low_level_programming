8c403ad86a3c2bb28259652a416b10353549671a
abel <berhanuabel@gmail.com>
2022-09-19 11:15:48 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,9 +7,11 @@
 
 void puts2(char *str)
 {
-	while (*str)
-		if (str % 2 == 0)
-			_putchar(*str);
-		*str++;
+	int i = 0;
+
+	while (*(str + i) != '\0')
+		if (i % 2 == 0)
+			_putchar(*(str + i);
+		i++;
 	_putchar('\n');
 }
