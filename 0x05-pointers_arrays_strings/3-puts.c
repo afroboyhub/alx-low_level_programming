eba673377ad0471b5502ace04ab95cbb69d6e9be
abel <berhanuabel@gmail.com>
2022-09-19 09:52:43 -0700

    task3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,9 +2,12 @@
 
 /**
  * _puts - prints a string followd by a new line to stdout
- * @*str: parameter
+ * @str: string of characters
  */
 
 void _puts(char *str)
 {
-
+	while (*str)
+		_putchar(*str++);
+	_putchar('\n');
+}
