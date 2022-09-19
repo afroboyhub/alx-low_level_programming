bc205e746023421d6483ab623379a80aaf4eafc7
abel <berhanuabel@gmail.com>
2022-09-19 12:49:38 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,9 +11,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
 
-	while (src[index])
-		dest[index] = src[index];
+	while (*src != '\0')
+
+		dest[index] = *src;
 		index++;
+		src++;
 	
 		return (dest);
 }
