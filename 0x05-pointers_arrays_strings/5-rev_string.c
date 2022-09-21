cbd5c872cbb69ca611e7c9ce562ee1dc4e27b186
abel <berhanuabel@gmail.com>
2022-09-21 05:17:57 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,21 +7,12 @@
 
 void rev_string(char *s)
 {
-	char tmp;
-	in i, len1, len2;
+	int i, tmp, len = _strlen(s);
 
-	len1 = 0;
-	len2 = 0;
-
-	while (s[len1] != '\0')
-		len1++;
-
-	len2 = len1 - 1;
-	for (i = 0; i < len1 / 2; i ++)
+	for (i = 0; i < len / 2; i++)
 	{
-		tmp = s[i];
-		s[i] = s[len2];
-		s[len2] = tmp;
-		len2 -= 1;
+		tmp = *(s + i);
+		*(s + i) = *(s + len - i - 1);
+		*(s + len - i - 1) = tmp;
 	}
 }
