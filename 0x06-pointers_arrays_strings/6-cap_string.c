01b098edd6086e83fdc1c54a5581ad0a2830b87c
abel <berhanuabel@gmail.com>
2022-09-21 18:56:45 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,23 +13,24 @@ char *cap_string(char *s)
 	while (s[index])
 	{
 		while (!(s[index] >= 'a' && s[index] <= 'z'))
+		{
 			index++;
-
+		}
 		if (s[index - 1] == ' ' ||
-			s[index - 1] == '\t' ||
-			s[index - 1] == '\n' ||
-			s[index - 1] == ',' ||
-			s[index - 1] == ';' ||
-			s[index - 1] == '.' ||
-			s[index - 1] == '!' ||
-			s[index - 1] == '?' ||
-			s[index - 1] == '"' ||
-			s[index - 1] == '(' ||
-			s[index - 1] == ')' ||
-			s[index - 1] == '{' ||
-			s[index - 1] == '}' ||
-			index == 0)
-				s[index] -= 32;
+		s[index - 1] == '\t' ||
+		s[index - 1] == '\n' ||
+		s[index - 1] == ',' ||
+		s[index - 1] == ';' ||
+		s[index - 1] == '.' ||
+		s[index - 1] == '!' ||
+		s[index - 1] == '?' ||
+		s[index - 1] == '"' ||
+		s[index - 1] == '(' ||
+		s[index - 1] == ')' ||
+		s[index - 1] == '{' ||
+		s[index - 1] == '}' ||
+		index == 0)
+			s[index] -= 32;
 
 		index++;
 	}
