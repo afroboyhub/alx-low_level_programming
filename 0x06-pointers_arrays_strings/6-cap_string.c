8ec61c4f5efcaf79465ad3de4f5b574732b07c98
abel <berhanuabel@gmail.com>
2022-09-21 18:52:40 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,36 @@
 
 /**
  * *cap_string - capitalizes all words of a string
+ * @s: string to be processed
+ * Return: pointer to modified string
  */
 
-char *cap_string(char *)
+char *cap_string(char *s)
 {
+	int index = 0;
+
+	while (s[index])
+	{
+		while (!(s[index] >= 'a' && s[index] <= 'z'))
+			index++;
+
+		if (s[index - 1] == ' ' ||
+			s[index - 1] == '\t' ||
+			s[index - 1] == '\n' ||
+			s[index - 1] == ',' ||
+			s[index - 1] == ';' ||
+			s[index - 1] == '.' ||
+			s[index - 1] == '!' ||
+			s[index - 1] == '?' ||
+			s[index - 1] == '"' ||
+			s[index - 1] == '(' ||
+			s[index - 1] == ')' ||
+			s[index - 1] == '{' ||
+			s[index - 1] == '}' ||
+			index == 0)
+				s[index] -= 32;
+
+		index++;
+	}
+	return (s);
+}
