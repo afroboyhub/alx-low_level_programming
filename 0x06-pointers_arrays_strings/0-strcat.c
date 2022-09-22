292fad54a45c41ea6facb70a4161fe921cb90cb4
abel <berhanuabel@gmail.com>
2022-09-21 17:06:37 -0700

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,7 +14,7 @@ char *_strcat(char *dest, char *src)
 	while (dest[i++])
 		dest_len++;
 	for (i = 0; src[i]; i++)
-		dest[dest_len++} = src[i];
+		dest[dest_len ++} = src[i];
 
 	return (dest);
 }
