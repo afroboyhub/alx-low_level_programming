f887d3ce903c9f6c361babf53031e05edb18c59a
abel <berhanuabel@gmail.com>
2022-09-21 18:03:10 -0700

    task4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,3 +8,16 @@
 
 void reverse_array(int *a, int n)
 {
+	int i, p;
+	int tmp;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		for (p = i + 1; p > 0; p--)
+		{
+			tmp = *(a + p);
+			*(a + P) = *(a + (p - 1));
+			*(a + (p - 1)) = tmp;
+		}
+	}
+}
