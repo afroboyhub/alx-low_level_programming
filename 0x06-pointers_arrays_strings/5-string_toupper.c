7b4b09ae4578bfdf4586678f7a3293ef50993660
abel <berhanuabel@gmail.com>
2022-09-21 18:20:25 -0700

    task5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,8 +1,21 @@
-#include "man.h"
+#include "main.h"
 
 /**
  * *string_toupper - function that changes all lc letters of a string to uc
+ * @s: string to process
+ * Return: to pointer dest
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *s)
 {
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		if ((*(s + count) >= 97) && (*(s + count) <= 122))
+			*(s + count) = *(s + count) - 32;
+		count++;
+	}
+
+	return (s);
+}
