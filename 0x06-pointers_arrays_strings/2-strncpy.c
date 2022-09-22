66f5add01c40883b4ba660518078b0d140ade866
abel <berhanuabel@gmail.com>
2022-09-21 17:42:45 -0700

    task2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,7 +5,24 @@
  * @dest: destination file
  * @src: source file
  * @n: number of bytes
+ * Return: pointer to dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
