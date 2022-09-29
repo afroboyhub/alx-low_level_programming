25775efff315cd6631b2338b4d88af844df57c28
abel <berhanuabel@gmail.com>
2022-09-29 13:21:42 -0700

    mandatory

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * is_palindrome - returns 1 if string is a palindrome
+ * @s: string to be checked
+ */
+
+int is_palindrome(char *s)
+{
---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings and returns 1 if string are identical
+ * @s1: first string to be compared
+ * @s2: second string to be compared
+ */
+
+int wildcmp(char *s1, char *s2)
+{
