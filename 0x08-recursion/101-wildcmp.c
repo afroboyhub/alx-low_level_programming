72282847522c3ec5d7fa80c691358bf1a0c4a298
abel <berhanuabel@gmail.com>
2022-09-29 13:31:11 -0700

    task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -4,7 +4,17 @@
  * wildcmp - compares two strings and returns 1 if string are identical
  * @s1: first string to be compared
  * @s2: second string to be compared
+ *
+ * Return: 1 if identical, 0 otherwise
  */
 
 int wildcmp(char *s1, char *s2)
 {
+	if (*s2 == '\0')
+		return (*s1 == '\0');
+	if (*s2 == *s1)
+		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+	return (0);
+}
