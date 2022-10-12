6c586f101720bd58f8e1f309ec5fc5109aa1592e
abel <berhanuabel@gmail.com>
2022-10-12 13:31:16 -0700

    fpointers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments given to program
+ * @argv: array of pointers to arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, int *argv[])
+{
+	int bytes, i;
+	int (*address)(int, char **) = main;
+	unsigned char opcode;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < bytes; i++)
+	{
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
+
+		if (i == bytes - 1)
+			continue;
+		printf(" ");
+
+		address++;
+	}
+
+	printef("\n");
+	return (0);
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: the array to be searched
+ * @size: the size of the array
+ * @cmp: a pointer to the function
+ *
+ * Return: index of the first element. no element or size <=0 return -1.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+	for (index = 0; index < size; index++)
+	{
+		if (cmp(array[index]) != 0)
+			return (index);
+	}
+
+	return (-1);
+}
