fd9d321c0b50770eab8952e4333a56e476625881
Abel <berhanuabel@gmail.com>
2022-10-31 14:28:57 -0700

    file i/o

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: a pointer to name of file
+ * @letters: the number of letters func should read
+ *
+ * Return: NULL if func fails or numb of bytes of func
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *buff;
+
+	if (filename == NULL)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buff, letters);
+	w = write(STDOUT_FILENO, buff, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buff);
+		return (0);
+	}
+
+	free(buff);
+	close(o);
+
+	return (w);
+}
